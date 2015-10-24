b32f1dd0f9f6279434ec34eb369bd248e97493f5
Alexander Marshall <apmarshall@soren.tech>
2015-10-24 17:19:18 -0400

    Install script is now just for creating init.el file

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,45 +1,5 @@
-# Install emacs via Homebrew, replace sytem version
-
-echo "Installing Emacs from Homebrew"
-brew install emacs --with-cocoa
-
-# Tap Homebrew Emacs Keg
-
-echo "Tapping Emacs Keg"
-brew tap homebrew/emacs
-
 # Create Emacs init.el file
 echo '(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
   (normal-top-level-add-subdirs-to-load-path))' > ~/.emacs.d/init.el
-
-# Install Emacs Customization Packages
-
-echo "Installing Emacs Packages"
-
-echo "Magit"
-brew install magit
-
-echo "Flycheck"
-brew install flycheck
-
-echo "Smex"
-brew install smex
-
-echo "Markdown Mode"
-brew install markdown-mode
-
-echo "PHP Mode"
-brew install php-mode
-
-echo "Web Mode"
-brew install web-mode
-
-echo "Helm"
-brew install helm
-
-# Finish up
-brew cleanup
-
-echo "All Emacs extensions installed!"
 
 exit 0
