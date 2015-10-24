919c565e21045790df74a73ba49528e883167787
Alexander Marshall <apmarshall@soren.tech>
2015-10-24 00:22:44 -0400

    Make the install script more verbose.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -28,22 +28,24 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Make sure we’re using the latest Homebrew.
+echo "Making sure Homebrew is up to date."
 brew update
 
 # Upgrade any already-installed formulae.
+echo "Updating existing formulae."
 brew upgrade --all
 
 # Tap appropriate kegs
+echo "Tapping needed kegs."
 brew tap homebrew/dupes
 brew tap homebrew/versions
 brew tap homebrew/homebrew-php
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+echo "Installing Utilities."
 brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
-# Install some other useful utilities like `sponge`.
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
@@ -53,6 +55,7 @@ brew install gnu-sed --with-default-names
 brew install wget --with-iri
 
 # Install more recent versions of some OS X tools.
+echo "Installing more recent version of some OS X tools."
 brew install grep
 brew install openssh
 brew install screen
@@ -60,15 +63,16 @@ brew install php56
 brew install git
 
 # Install Add-on Tools
+echo "Installing extensions to tools you use."
 brew install git-extras
 brew install fasd
 brew install bundler
 brew install hub
-brew install imagemagick
 brew install tmux
 brew install the_silver_searcher
 
 # Install Web Development Tools
+echo "Installing web development tools."
 brew install awscli
 brew install brew-php-switcher
 brew install composer
@@ -81,7 +85,13 @@ brew install redis
 brew install ruby
 brew install wp-cli
 
+# Install Other Packages
+echo "Installing a few other packages."
+brew install imagemagick
+
 # Remove outdated versions from the cellar.
 brew cleanup
+
+echo "All packages installed."
 
 exit 0
