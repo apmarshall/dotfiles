bf9dba3a4464aa3a4473af7f5ce6fe349c28f3dc
apmarshall <apmarshall@soren.tech>
2015-10-23 22:41:54 -0400

    Set default shell to Zsh

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -390,6 +390,9 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 # Terminal & iTerm 2                                                          #
 ###############################################################################
 
+# Set default shell to Zsh
+chsh -s $(which zsh)
+
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
 
