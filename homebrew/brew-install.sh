e782371f536d650321c933c60a17297c403e4f11
Alexander Marshall <apmarshall@soren.tech>
2015-10-25 13:17:27 -0400

    Commenting out the stuff we know works to test the part we don't

--- homebrew/brew-install.sh
+++ homebrew/brew-install.sh
@@ -8,40 +8,40 @@
 set -e
 
 # Ask for the administrator password
- sudo -v
+# sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
- while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Fix permissions issues that might stop Homebrew from working
-echo "Checking that Homebrew has the permissions it needs to work"
-echo "Checking usr/local"
-sudo chown -R $(whoami):admin /usr/local
+#echo "Checking that Homebrew has the permissions it needs to work"
+#echo "Checking usr/local"
+#sudo chown -R $(whoami):admin /usr/local
 
 # Check for Homebrew
-echo "Checking for Homebrew."
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew for you."
+#echo "Checking for Homebrew."
+#if test ! $(which brew)
+#then
+#  echo "  Installing Homebrew for you."
 
   # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-  fi
-  
-fi
-echo "Homebrew is installed."
+# if test "$(uname)" = "Darwin"
+#  then
+#    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+#  then
+#    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+#  fi
+#  
+#fi
+#echo "Homebrew is installed."
 
-echo "Checking permissions for the Homebrew cache"
-sudo chown -R $(whoami):admin $(brew --cache)
+#echo "Checking permissions for the Homebrew cache"
+#sudo chown -R $(whoami):admin $(brew --cache)
 
 # Make sure we’re using the latest Homebrew.
-echo "Making sure Homebrew is up to date."
-brew update
+#echo "Making sure Homebrew is up to date."
+#brew update
 
 # Check that jq is installed. If not, install it.
 echo "Checking for needed packages"
