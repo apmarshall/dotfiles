fd6317738914f307de7c7b0097386f4a30714c5e
Alexander Marshall <apmarshall@soren.tech>
2015-10-26 23:10:50 -0400

    Caught two more sudo required preferences, got rid of them

--- osx/set-defaults-stanu.sh
+++ osx/set-defaults-stanu.sh
@@ -336,10 +336,6 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 #	'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
 # Load new settings before rebuilding the index
 #killall mds > /dev/null 2>&1
-# Make sure indexing is enabled for the main volume
-#sudo mdutil -i on / > /dev/null
-# Rebuild the index from scratch
-#sudo mdutil -E / > /dev/null
 
 ###############################################################################
 # Terminal & iTerm 2                                                          #
