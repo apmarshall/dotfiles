45be31d2ea128d4d4f7bcabd053d032e23ebf1dd
Alexander Marshall <apmarshall@soren.tech>
2015-10-26 22:04:46 -0400

    Commenting out temporarily

--- emacs/path.zsh
+++ emacs/path.zsh
@@ -1,3 +1,3 @@
-LATESTEMACS=$(brew ls --versions emacs)
+#LATESTEMACS=$(brew ls --versions emacs)
 
-alias emacs="/usr/local/Cellar/emacs/{$LATESTEMACS}/Emacs.app/Contents/MacOS/Emacs -nw"
+#alias emacs="/usr/local/Cellar/emacs/{$LATESTEMACS}/Emacs.app/Contents/MacOS/Emacs -nw"
