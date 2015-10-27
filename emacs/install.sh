d5e5882bf624598e91bc721999e9607e36c62be2
apmarshall <apmarshall@soren.tech>
2015-10-27 19:10:23 -0400

    Removing solarized emacs, it looks awful

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,9 +1,5 @@
 # Create Emacs init.el file
-echo '(let ((default-directory "/usr/local/share/emacs/site-lisp/")) \
+echo '(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
   (normal-top-level-add-subdirs-to-load-path))' > ~/.emacs.d/init.el
-
-echo '(add-to-list 'custom-theme-load-path "/usr/local/Cellar/solarized-emacs/HEAD/share/emacs/site-lisp/solarized") \
-(load-theme 'solarized t) \
-(setq solarized-termcolors 256)' > ~/.emacs.d/init.el
 
 exit 0
