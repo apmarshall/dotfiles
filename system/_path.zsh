e3aabdfd1f0703870b27e466b3dffa60b39c1e90
Alexander Marshall <apmarshall@soren.tech>
2015-10-27 23:01:44 -0400

    Adding Homebrew installed coreutils to the path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
