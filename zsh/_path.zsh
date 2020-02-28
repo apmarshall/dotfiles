7eb47b52e7ef84757f1c96f87346aab4c73f66fb
Alex Floyd Marshall <apmarshall@soren.tech>
2020-02-27 20:06:06 -0500

    Fix: Create shortcut to dotfiles directory
    We need this for the PATH variable.

--- zsh/_path.zsh
+++ zsh/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/opt/coreutils/libexec/gnubin:${HOME}/.config/yarn/global/node_modules/.bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOT/bin:/usr/local/opt/coreutils/libexec/gnubin:${HOME}/.config/yarn/global/node_modules/.bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
