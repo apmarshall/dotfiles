abb15f064bec6b078416ce9f232f712e0db8ec2f
Alex Floyd Marshall <apmarshall@soren.tech>
2025-09-19 16:19:19 -0400

    Apple silicon homebrew path

--- dot_zsh/_path.zsh
+++ dot_zsh/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/bin:/usr/local/opt/coreutils/libexec/gnubin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/Library/Python/3.9/bin:$PATH"
+export PATH="./bin:/opt/homebrew/bin:/usr/local/bin:/usr/local/sbin:$HOME/bin:/usr/local/opt/coreutils/libexec/gnubin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/Library/Python/3.9/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
